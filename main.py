18a2b5f87adc6ec64007ff443267f0911e09af89
test@gmail.com <test@gmail.com>
2020-06-15 22:28:32 +0300

    prog exit li name of cat

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-cat_names = []
-
-while True:
-    print('Enter name of cat' + str( len(cat_names) + 1 ) +
-        'or press enter to stop')
-    name = input()
-    if name == '':
-        break
-    cat_names += [name]
-
-print('the cats names are:')
-for name in cat_names:
-    print(' ' + name)
+my_pets = ['bicha', 'zoee', 'chybasi', 'serun']
+name = input('Enter a pet name: ')
+if name not in my_pets:
+    print(name,'Does not exit!')
+else:
+    print(name, ' is my pet')
