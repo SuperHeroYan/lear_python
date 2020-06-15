c0cecfe664fbed4b7b82bde14ca7f5f0b622630a
test@gmail.com <test@gmail.com>
2020-06-15 21:05:44 +0300

    add cirle

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def collatz(num):
     if num % 2 == 0:
         print('Число ',num,' четное')
+        return num % 2
     elif num % 2 == 1:
         print('Число ',3 * num + 1,' нечетное')
+        return 3 * num + 1
 
 print('Введите число')
-answer = int(input())
 
-collatz(answer)
+res = 0
+
+while res != 1:
+    answer = int(input())
+    res = collatz(answer)
