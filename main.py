ea75be7e34af33499f124139fa2ffa03d4e53155
test@gmail.com <test@gmail.com>
2020-06-22 21:21:11 +0300

    make databases birthdays

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-grid =[['.', '.', '.', '.', '.', '.'],
-       ['.', 'O', 'O', '.', '.', '.'],
-       ['O', 'O', 'O', 'O', '.', '.'],
-       ['O', 'O', 'O', 'O', 'O', '.'],
-       ['.', 'O', 'O', 'O', 'O', 'O'],
-       ['O', 'O', 'O', 'O', 'O', '.'],
-       ['O', 'O', 'O', 'O', '.', '.'],
-       ['.', 'O', 'O', '.', '.', '.'],
-       ['.', '.', '.', '.', '.', '.']]
+birthdays = {'olga': '7 june', 'petr': '21 june'}
 
-for i in range(len(grid[0])):
-    
-    for j in range(len(grid)):
-        print(grid[j][i], end='')
+while True :
+    name = input('Enter a name: (blank to quit)')
+    if name == '':
+        break
 
-    print()
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        bday = input('You wanna do add birthday in base of birthdays? ' +
+                    '(blank to quit) ')
+        if bday == '':
+            break
+        else:
+            birthdays[name] = bday
+            print('Greate, base update!')
