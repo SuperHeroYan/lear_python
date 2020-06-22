9be5e399281701ab3c11a35505b12b0dbd0525dd
test@gmail.com <test@gmail.com>
2020-06-22 22:27:29 +0300

    make a prog which make count char

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-birthdays = {'olga': '7 june', 'petr': '21 june'}
+import pprint 
+message = 'It was a cold fck day '
+count = {}
 
-while True :
-    name = input('Enter a name: (blank to quit)')
-    if name == '':
-        break
+for char in message:
+    count.setdefault(char, 0)
+    count[char] = count[char] + 1
 
-    if name in birthdays:
-        print(birthdays[name] + ' is the birthday of ' + name)
-    else:
-        print('I do not have birthday information for ' + name)
-        bday = input('You wanna do add birthday in base of birthdays? ' +
-                    '(blank to quit) ')
-        if bday == '':
-            break
-        else:
-            birthdays[name] = bday
-            print('Greate, base update!')
+pprint.pprint(count)
