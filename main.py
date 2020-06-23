525d34f5d4b01ef949212cd65b6a16463fc9287c
test@gmail.com <test@gmail.com>
2020-06-23 19:17:01 +0300

    finish X&O game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-the_board = {'top-L': '0', 'top-M': '0', 'top-R': '0',
-            'mid-L': 'x', 'mid-M': 'x', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': 'x'}
+the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
 
 def print_board(board):
@@ -9,5 +9,15 @@ def print_board(board):
     print( board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-+-+-')
     print( board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+for i in range(9):
+    print_board(the_board)
+    move = input('Turn for ' + turn + ' . Move on which space ?')
+    the_board[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
 
 print_board(the_board)
