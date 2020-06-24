27dc5925d407753f4c5bcd62bfc6c482a8476bdb
test@gmail.com <test@gmail.com>
2020-06-24 22:35:14 +0300

    Create execute sql func

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import sqlite3
 from sqlite3 import Error
 
+create_users_table = """
+CREATE TABLE IF NOT EXISTS `users` (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    gender TEXT,
+    nationality TEXT
+);
+"""
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+"""
+
+# Соединение с ьд
 def create_connection(path):
     connection = None
     try:
@@ -11,3 +32,17 @@ def create_connection(path):
         print(f"The error '{e}' occurred")
 
     return connection
+
+# Выполняет запрос для бд
+def execute_query(connection, query):
+    # Хуйня что бы вызвать мето execute который выполнеяет запрос
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print('Query executed successfully')
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+connect = create_connection('first_db')
+execute_query(connect, create_posts_table)
