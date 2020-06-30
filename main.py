e4431a169509129cb6e6f37968924ff68622d445
test@gmail.com <test@gmail.com>
2020-06-30 22:18:46 +0300

    taste isX() method

--- main.py
+++ main.py
@@ -1,66 +1,15 @@
-import sqlite3
-from sqlite3 import Error
+while True:
+    print('Entert your age:')
+    age = input()
+    # Проверяет что бы были только цифры
+    if age.isdecimal():
+        break
+    print('Please enter a number for you age')
 
-create_users_table = """
-CREATE TABLE IF NOT EXISTS `users` (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    age INTEGER,
-    gender TEXT,
-    nationality TEXT
-);
-"""
-
-create_posts_table = """
-CREATE TABLE IF NOT EXISTS posts(
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  title TEXT NOT NULL, 
-  description TEXT NOT NULL, 
-  user_id INTEGER NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id)
-);
-"""
-
-create_comments_table = """
-CREATE TABLE IF NOT EXISTS comments (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  text TEXT NOT NULL, 
-  user_id INTEGER NOT NULL, 
-  post_id INTEGER NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-);
-"""
-
-create_likes_table = """
-CREATE TABLE IF NOT EXISTS likes (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  user_id INTEGER NOT NULL, 
-  post_id integer NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
-);
-"""
-
-# Соединение с ьд
-def create_connection(path):
-    connection = None
-    try:
-        # Подлючается к базе данных 
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-# Выполняет запрос для бд
-def execute_query(connection, query):
-    # Хуйня что бы вызвать мето execute который выполнеяет запрос
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print('Query executed successfully')
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-
-connect = create_connection('first_db')
+while True:
+    print('Select a new password (letters and numbers only)')
+    password = input()
+    # Что бы были только числа и буквы
+    if password.isalnum():
+        break
+    print('Password can only have letters and numbers.')
