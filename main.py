dc1d7420dc232047280186b0d0168b44d5833bb4
test@gmail.com <test@gmail.com>
2020-07-01 21:49:43 +0300

    add * before start sthrins

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-# pw.py - Программа для незащищенного хранения паролей
+# pw.py - Программа для  установки звездочек
 import sys, pyperclip
 
-PASSWORDS = {
-            'email': 'goper@gmail.com',
-            'blog': 'valmort_228',
-            'luggage': '12343'}
+text = pyperclip.paste()
+# Разделяет строку на список и добавляет звезды в начало
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
 
-code_access = '228'
-
-while True:
-    user_access = input('Введите пароль!\n')
-    if user_access == code_access:
-        break
-   
-    print('Пароль не верен!')
-
-if len(sys.argv) < 2:
-    print('Использоование: python pw.py [имя учетной записи] - \
-        копирование пароля учетной записи')
-    sys.exit()
-
-account = sys.argv[1] # Первый аргумент командной строки это имя учет.записи
-
-if account in PASSWORDS:
-    # Копирует в буфер обмена 
-    pyperclip.copy(PASSWORDS[account])
-    print('Пароль для ' + account + ' скопирован в буфер')
-else:
-    print('Учетная запись ' + account + ' отсутствует в списке')
+text = '\n'.join(lines)
+pyperclip.copy(text)
