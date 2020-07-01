1427b46f7951eba8a9b163f75c680f19424bfe80
test@gmail.com <test@gmail.com>
2020-07-01 20:55:04 +0300

    Create manager passwords

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-def printPicnic(items, left, right):
-    print('PICNIC ITEMS'.center(left + right, '-'))
-    for k, v in items.items():
-        print(k.ljust(left,'.') + str(v).rjust(right))
+# pw.py - Программа для незащищенного хранения паролей
+import sys, pyperclip
 
-picitems = {'sand': 5, 'apples': 22, 'cips': 55}
-printPicnic(picitems, 12 ,6)
+PASSWORDS = {
+            'email': 'goper@gmail.com',
+            'blog': 'valmort_228',
+            'luggage': '12343'}
+
+code_access = '228'
+
+while True:
+    user_access = input('Введите пароль!\n')
+    if user_access == code_access:
+        break
+   
+    print('Пароль не верен!')
+
+if len(sys.argv) < 2:
+    print('Использоование: python pw.py [имя учетной записи] - \
+        копирование пароля учетной записи')
+    sys.exit()
+
+account = sys.argv[1] # Первый аргумент командной строки это имя учет.записи
+
+if account in PASSWORDS:
+    # Копирует в буфер обмена 
+    pyperclip.copy(PASSWORDS[account])
+    print('Пароль для ' + account + ' скопирован в буфер')
+else:
+    print('Учетная запись ' + account + ' отсутствует в списке')
