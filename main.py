7614c3baf47c07d8d577b8b7a33c93cc4d881ded
test@gmail.com <test@gmail.com>
2020-07-02 21:08:29 +0300

    checker number phone

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-# pw.py - Программа для  установки звездочек
-import sys, pyperclip
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
 
-text = pyperclip.paste()
-# Разделяет строку на список и добавляет звезды в начало
-lines = text.split('\n')
-for i in range(len(lines)):
-    lines[i] = '* ' + lines[i]
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+        
+    if text[3] != '-':
+        return False
 
-text = '\n'.join(lines)
-pyperclip.copy(text)
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+
+    if text[7] != '-':
+        return False
+
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+
+    return True
+
+print(isPhoneNumber('4ss-123-3333'))
