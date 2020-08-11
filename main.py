3e0d19f24dbbe03e2bfb53aec0179bbbd79bc849
test@gmail.com <test@gmail.com>
2020-08-11 22:37:55 +0300

    sex

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-def isPhoneNumber(text):
-    if len(text) != 12:
-        return False
-
-    for i in range(0, 3):
-        if not text[i].isdecimal():
-            return False
-        
-    if text[3] != '-':
-        return False
-
-    for i in range(4, 7):
-        if not text[i].isdecimal():
-            return False
-
-    if text[7] != '-':
-        return False
-
-    for i in range(8, 12):
-        if not text[i].isdecimal():
-            return False
-
-    return True
-
-print(isPhoneNumber('4ss-123-3333'))
+import re
+bat_regex = re.compile(r'Bat(wo)*man')
+m1 = bat_regex.search('The Adventure of Batman')
+m1.group()
