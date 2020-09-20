0e9bbdd6cc5796b54209983f30d69027c0f874be
test@gmail.com <test@gmail.com>
2020-09-20 21:37:48 +0300

    make buffer saver prog

--- main.py
+++ main.py
@@ -1,65 +1,50 @@
-# Рандомайзер для экзамена по географии
-import random
+# mcb.pyw - Сохраняет и загружает фрагменты текста в буфер
+# Использование: py.exe mcb.pyw save <ключение_слово> -
+# 				Сохраняет буфер обмена в ключевом словое.
+#				py.exe mcb.pyw <ключение_слово>
+#					Загружает ключ.слово в буфер обмена 
+#					py.exe mcb.pyw list - 
+#					Загружает все ключевые слов в буфер
 
-# Ключи назвагте штатов а значения столицы
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-            'Arkansas': 'Little Rock', 'California': 'Sacramento',
-            'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover',
-            'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu',
-            'Idaho': 'Boie', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis',
-            'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort',
-            'Louisina': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
-            'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul',
-            'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena',
-            'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord',
-            'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany',
-            'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus',
-            'Oaklahoma': 'Oaklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg',
-            'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
-            'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin',
-            'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond',
-            'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison',
-            'Wyoming': 'Cheyenne'}
+import shelve, pyperclip, sys
+mcbShelf = shelve.open('mcb')
 
-# Генерация 35 файлов билетов
-for quizNum in range(35):
+# Сохранение в буфер
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+	mcbShelf[sys.argv[2]] = pyperclip.paste()
+	print('Your data buffer successyfyly copy to database')
+	mcbShelf.close()
 
-	# Создагте билетов и ответов к ним
-	quizFile = open('cap/capitalsquiz%s.txt' % (quizNum + 1), 'w')
-	answerKeyFile = open('cap/capitalsquiz_answer%s.txt' % (quizNum + 1), 'w')
+# вывод всех данных и бд
+elif len(sys.argv) == 2 and sys.argv[1].lower() == 'show':
+	print('List of Db:')
+	for key in mcbShelf:
+		print('[',key,']', '-', mcbShelf[key])
 
-	#Запись заголовка билетов
-	quizFile.write('Имя:\n\nДата:\n\nКурс:\n\n')
-	quizFile.write( (' ' * 15) + 'Проверка знаний столиц штатов (Билет %s)'
-				% (quizNum + 1))
-	quizFile.write('\n\n')
+# Копирует содержимое базы данаах в виде списка в буфер
+elif len(sys.argv) == 2 and sys.argv[1].lower() == 'list':
+	pyperclip.copy(str(list(mcbShelf.keys())))
+	print('List of records successyfyly copy to bufer')
+	mcbShelf.close()
 
-	#Перемешивание порядка следования столиц щтаово
-	states = list(capitals.keys())
-	random.shuffle(states)
+#Берет значение из хранилища и помещает в буфер
+elif sys.argv[1] in mcbShelf:
+		pyperclip.copy(mcbShelf[sys.argv[1]])
+		print('Your record successyfyly copy in bufer')
+		mcbShelf.close()
 
-	#Цикл по всем штататм с созданием вопроса к кажому
-	for questionNum in range(50):
+# Удаление записей из бд
+elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+	del mcbShelf[sys.argv[2]]
+	print('Record successyfyly deleted')
+	mcbShelf.close()
 
-		#Получение прав и неправ ответов
-		correctAnswer = capitals[states[questionNum]]
-		wrongAnswer = list(capitals.values())
-		del wrongAnswer[wrongAnswer.index(correctAnswer)]
-		wrongAnswer = random.sample(wrongAnswer, 3)
-		answerOptions = wrongAnswer + [correctAnswer]
-		random.shuffle(answerOptions)
-		
-		#Запись вариантов ответов в файл
-		quizFile.write('%s. Выберите столицу штата %s.\n' % 
-			(questionNum + 1, states[questionNum]))
-		quizFile.write('\n')
+# Удаление всех записей из бд
+elif len(sys.argv) == 2 and sys.argv[1].lower() == 'clear':
+	mcbShelf.clear()
+	print('All record successyfyly deleted')
+	mcbShelf.close()
 
-		for i in range(4):
-			quizFile.write('%s. %s\n' % ('ABCD'[i], answerOptions[i]))
-			quizFile.write('\n')
-
-		#Запись ключа ответа в файл.
-		answerKeyFile.write('%s. %s\n' % ((questionNum + 1), 'ABCD'[answerOptions.index(correctAnswer)]))
-
-quizFile.close()
-answerKeyFile.close()
+else:
+	print('Error command, try again')
+	mcbShelf.close()
