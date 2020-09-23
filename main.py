85e5defe0f1560f4780427256c8f0651b2a14d69
test@gmail.com <test@gmail.com>
2020-09-23 22:55:37 +0300

    make prog which make search words in files

--- main.py
+++ main.py
@@ -1,46 +1,21 @@
-# Madlibs prog
+import os
 import re
-# открываем файл для чтения и вывлда контета пользователю
-textfilecontent = open('keks.txt')
-content = textfilecontent.read()
-print(content)
 
-find_adj = re.compile(r'ADJECTIVE')
-# Проверяем есть ли в контенте ADJECTIVE если есть то запускаеться цикл
-starteradj = bool(find_adj.search(content))
-counteradj = 0
+answer_user = input('Какое слово хотите искать?\n')
 
-while starteradj == True:
-	counteradj += 1
-	adjin = input(f'Введите {counteradj} прилагательное: \n')
-	content = re.sub(find_adj, adjin, content, 1)
-	starteradj = bool(find_adj.search(content))
-	print('\n',content)
+for file in os.listdir('./'):
+	if file.endswith('.txt'):
 
-find_noun = re.compile(r'NOUN')
+		claypath = os.path.join('./', file)
+		opened_file = open(claypath, 'r')
+		content = opened_file.read()
 
-starternoun = bool(find_noun.search(content))
-counternoun = 0
+		regex_patter = re.compile(rf'{answer_user}')
+		result = re.findall(regex_patter, content)
 
-while starternoun == True:
-	counternoun += 1
-	nounin = input(f'Введите {counternoun} существительное: \n')
-	content = re.sub(find_noun, nounin, content, 1)
-	starternoun = bool(find_noun.search(content))
-	print('\n',content)
-
-find_verb = re.compile(r'VERB')
-
-starterverb = bool(find_verb.search(content))
-counterverb = 0
-
-while starterverb == True:
-	counterverb += 1
-	verbin = input(f'Введите {counterverb} глагол: \n')
-	content = re.sub(find_verb, verbin, content, 1)
-	starterverb = bool(find_verb.search(content))
-	print('\n',content)
-
-
-textfilecontent = open('keks.txt', 'w')
-textfilecontent.write(content)
+		if result != [] and result != '':
+			print(f'------------------------\nСлово найдено в файле{claypath}\n')
+			print(content,'\n------------------------\n')
+		else:
+			print(f'- {claypath}')
+		
