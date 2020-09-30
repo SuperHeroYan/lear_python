fb7c0d155abbb68bf7695d932fa6acb92ba61576
test@gmail.com <test@gmail.com>
2020-09-30 21:14:35 +0300

    make renameDate progssss

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-import os
+# renameDates.py - переименовывает файлы, имена которых включат в себя даты амерк.типа
+# в евпорейский формат 
+import os, re, shutil
 
-for folderName, subfolders, filenames in os.walk('./cap'):
-	print('Текущая папка - ' + folderName)
+# Создание регулярки которое будут узнать имена файлов
+datePattern = re.compile(r"""^(.*?) # весь текст перед датой
+	( (0|1)? \d)- # одна или две цифры месяца
+	( (0|1|2|3)? \d)- # одна или две цифры числа
+	( (19|20) \d\d) # четыре цифры года
+	(.*?)$ # весь текст после даты
+	""", re.VERBOSE) 
 
-	for subfolder in subfolders:
-		print('Подпапка папки ' + folderName + ': ' + subfolder)
+for amerFilename in os.listdir('.'):
+	mo = datePattern.search(amerFilename)
 
-	for filename in filenames:
-		print('Файл в папке ' + folderName + ': ' + filename)
+	if mo == None:
+		continue
 
-	print('')
+	beforePart = mo.group(1)
+	monthPart = mo.group(2)
+	dayPart = mo.group(4)
+	yearPart = mo.group(6)
+	afterPart = mo.group(8)
+
+	# Формировние имени файла, по европейски
+	euroFilename = beforePart + dayPart + '-' + monthPart + \
+			'-' + yearPart + afterPart
+
+	# Получение полных абсолютеых путей к файлам
+	asbWorkDir = os.path.abspath('.')
+	amerFilename = os.path.join(asbWorkDir, amerFilename)
+	euroFilename = os.path.join(asbWorkDir, euroFilename)
+
+	# Переимнование файлов
+	print('Заменяем имя "%s" \nИменем "%s"'
+			% (amerFilename, euroFilename))
+
+	shutil.move(amerFilename, euroFilename)
+
