e520b0079169f3ccdaacb960d872331608fe65dd
test@gmail.com <test@gmail.com>
2020-10-24 01:09:42 +0300

    должно работать но неробит октрыеает 5 первых запросов в гугл

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 
-# mapInt prog запускает карту в браузере
+# Програма которая открывает несколько первых вкладок из google
 
-import sys, pyperclip
-import webbrowser
+import requests, sys, webbrowser, bs4
 
-if len(sys.argv) > 1:
-	addres = ' '.join(sys.argv[1:])
-else:
-	address = pyperclip.paste()
+print('Гуглим...')
 
-webbrowser.open('https://www.google.com/maps/place/' + address)
+res = requests.get('https://yandex.ru/search/?text=' +
+	' '.join(sys.argv[1:]))
+
+res.raise_for_status()
+
+# Извелечение первых ссылко
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+# Отдельное отркытие для каждой вкладки
+linkElems = soup.select('ul.search-result')
+numOpen = min(5, len(linkElems))
+
+
+for i in range(numOpen):
+	print('Открыта')
+	webbrowser.open('http://google.com' + linkElems[i].get('href'))
