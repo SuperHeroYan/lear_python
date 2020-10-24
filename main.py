f2d2df7f0dbeb1186902605897547531246c4248
test@gmail.com <test@gmail.com>
2020-10-24 21:41:42 +0300

    prog dowload image from Xkcd

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 
 # Програма которая открывает несколько первых вкладок из google
 
-import requests, sys, webbrowser, bs4
+import requests, os, webbrowser, bs4
 
-print('Гуглим...')
+url = 'https://xkcd.com/'
 
-res = requests.get('https://yandex.ru/search/?text=' +
-	' '.join(sys.argv[1:]))
-
-res.raise_for_status()
-
-# Извелечение первых ссылко
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-
-# Отдельное отркытие для каждой вкладки
-linkElems = soup.select('ul.search-result')
-numOpen = min(5, len(linkElems))
+os.makedirs('xkcd', exist_ok=True)
 
 
-for i in range(numOpen):
-	print('Открыта')
-	webbrowser.open('http://google.com' + linkElems[i].get('href'))
+while not url.endswith('#'):
+	print('Dowload %s...' % url)
+	res = requests.get(url)
+	res.raise_for_status()
+
+	soup = bs4.BeautifulSoup(res.text, 'lxml')
+	comicElem = soup.select('#comic img')
+
+	if comicElem == []:
+		print('Не удалось найти изображение комикс.')
+	else:
+		# Получаем ссылку на картинку
+		comicUrl = 'https:' + comicElem[0].get('src')
+		print('Загружается изображение %s...' % (comicUrl))
+		res = requests.get(comicUrl)
+		res.raise_for_status()
+		# Сохранение фалфа в папке
+		imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+
+		for chunk in res.iter_content(100000):
+			imageFile.write(chunk)
+		imageFile.close()
+
+	prevLink = soup.select('a[rel="prev"]')[0]
+	url = 'https://xkcd.com' + prevLink.get('href')
+
+print('Готово')
