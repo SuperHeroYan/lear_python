004401b5a2b5a991b66dd08c45172c5cbaa5fa3a
test@gmail.com <test@gmail.com>
2020-11-19 23:47:13 +0300

    PDF prog for unite files pfd format

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# CombinePdfs.py - Обьединяет все PDF документы находящиуся в текущем рабочем
+import PyPDF2 as pdf, os
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# Получение списка имен всех PDF-файлов
+pdfFiles = []
 
-browser = webdriver.Firefox()
-browser.get('https://vk.com/')
+for filename in os.listdir('.'):
+	if filename.endswith('.pdf'):
+		pdfFiles.append(filename)
 
-email = browser.find_element_by_id('index_email')
-password = browser.find_element_by_id('index_pass')
-button = browser.find_element_by_id('index_login_button')
+# Огранизация цикла по всем файлам
+pdfFiles.sort(key=str.lower)
+pdfWriter = pdf.PdfFileWriter()
 
-try:
-	print('Входим в аккаунт..')
-	email.send_keys('***')
-	password.send_keys('***')
-	button.click()
-	print('Добро пожаловать !')
-except:
-	print('Не удалось войти в аккаунт')
+for filename in pdfFiles:
+	pdffileObj = open(filename, 'rb')
+	pdfReader = pdf.PdfFileReader(pdffileObj)
+	# Проход цикла по всем станицам без первой
+	for pageNum in range(1, pdfReader.numPages):
+		pageObj = pdfReader.getPage(pageNum)
+		pdfWriter.addPage(pageObj)
 
-# Проверяет загрузилась ли страница
-try:
-	wait = WebDriverWait(browser, 10)
-	element = wait.until(EC.element_to_be_clickable((By.ID, 'l_msg')))
-except:
-	print('Не загрузилась')
-
-browser.get('https://vk.com/im')
-# Отправляем сообщение серьге
-chatid = browser.find_element_by_css_selector('[data-list-id="****"]')
-chatinput = browser.find_element_by_id('im_editable0')
-
-chatid.click()
-chatinput.send_keys('****')
-chatinput.send_keys(Keys.ENTER)
+# Сохранение результат
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
