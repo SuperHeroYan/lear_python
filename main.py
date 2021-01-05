7759eeda0bfe4d0f955c25bd8761b3a73aa2a65c
test@gmail.com <test@gmail.com>
2021-01-05 17:14:17 +0300

    Annotanation types

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
-import time, datetime
 
-halloween2020 = datetime.datetime(2021, 10, 31)
-while datetime.datetime.now() < halloween2020:
-	time.sleep(1)
+from types import Any, Union
+
+arrag: Any = "ase"
+
+arrage: list['str', 11] = ['sae', 22]
+sex: int = 11
+sherks: str = 'sex'
+lfds: None
+
+kop: float = 1.11
+
+def func(value:int, koper: list) -> Any:
+	pass
+
+
+def fun(arr: list[str, ...]) -> int:
+	pass
+
+def func(value: Union[str, int]) -> int:
+	pass
