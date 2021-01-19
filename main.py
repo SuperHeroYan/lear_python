bd9e1bf0fd8a2ea22f3350761bc5b04e249c34f4
example <example.com>
2021-01-19 17:41:13 +0300

    stopwatch_vers_2 use RJUST and LJust

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 
-"""countdown.py Простой сценарий обратного отсчета."""
+"""stopwatch_vers_2.py - Прогамма хронометр."""
 
-import time, subprocess
+import time
+import pyperclip
 
-timeLeft = 60
+# Отображение инструкции по использовани. прогаммы.
+print('Чтобы неачать отсчет, нажмите клавигу Enter. \nДля выхода <ctrl+c>')
+input()
 
-while timeLeft > 0:
-    print(timeLeft, end='')
-    time.sleep(1)
-    timeLeft = timeLeft - 1
+print('Отсчет начат')
+startTime = time.time()
 
-# Воспроизведение звук файла по оконачанию
-subprocess.Popen(['see', 'alarm.wav'])
+lastTime = startTime
+lapNum = 1
+
+# Отслежевание замеров
+try:
+    while True:
+        input()
+        lapTime = str(round(time.time() - lastTime, 2))
+        totalTime = str(round(time.time() - startTime, 2))
+
+        result = 'Замер # ' + str(lapNum) + ':' + \
+            totalTime.rjust(len(totalTime) + 2) + \
+            ' (' + lapTime.rjust(len(lapTime) + 4) + ')'
+        print(result)
+
+        lapNum += 1
+        lastTime = time.time()
+except KeyboardInterrupt:
+    pyperclip.copy(result)
+    print('\nГотово')
