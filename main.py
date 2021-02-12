e500f14275e11d5c12282f52b8d458dd86f659a4
example <example.com>
2021-02-12 17:53:01 +0300

    Mouse_now 2.0

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # mouse_now.py - Отображает текущее положение курсора
 import pyautogui
-
 print('Для выхода нажмите CNTRL+C')
 
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + \
-                      ' Y: ' + str(y).rjust(4)
+        pixel_color = pyautogui.screenshot().getpixel((x, y))
+
+        positionStr = f'X: {str(x).rjust(4)} Y: {str(y).rjust(4)} RGB: ({str(pixel_color[0]).rjust(3)}, {str(pixel_color[1]).rjust(3)}, {str(pixel_color[2]).rjust(3)})'
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
