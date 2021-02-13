c3ee88003670652950c6153ca038e216f148d2d8
example <example.com>
2021-02-13 19:16:47 +0300

    Manager context prog count time finish prog

--- main.py
+++ main.py
@@ -1,59 +1,23 @@
-def apply(product, discount):
-    price = int(product['цена']) * (1.0 - discount)
-    assert 0 <= price <= product['цена'], 'Цена ниже нуля'
-    print(price)
+import time
 
 
-shoes = {'Имя': 'Туфли', 'цена': 14900}
-
-apply(shoes, 2.0)
-
-# Бед практис всегда возращает Труе
-assert(100 == 111)
-
-me = ('fdsfdsfdsfdsfdsf ,'
-      'fsdfsdfdsfsdfsdfffffffffffffff'
-      'dfsfdsfsdfsd')
-
-
-# Использование Витх
-with open('hello.txt') as f:
-    f.close()
-
-
-# Менеджер контекста
-class Manage:
-    def __init__(self, name):
-        self.name
-
-    def __enter__(self):
-        self.file = open(self.name, 'w')
-        return self.file
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.file:
-            self.file.close()
-
-
-class Intendter:
+class TimeCount:
     def __init__(self):
-        self.level = 0
+        self.beginTime = 0
 
     def __enter__(self):
-        self.level += 1
+        self.beginTime = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.level -= 1
-
-    def print(self, text):
-        print(' ' * self.level + text)
+        self.result = time.time() - self.beginTime
+        print(f'Время выполнения: {self.result} микросек')
 
 
-with Intendter() as ints:
-    ints.print('Привет')
-    with ints:
-        ints.print('Хей')
-        with ints:
-            ints.print('Бонжур')
-    ints.print('Эй')
+asss = 11
+bs = 12
+
+with TimeCount() as timer:
+    concat = 'asd' + str(asss) + 'Bobeer ' + str(bs)
+    # concat = f'asd{str(asss)}Bobeer {str(bs)}'
+    print(concat)
